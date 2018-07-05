54ab55f04273016d78f5098be634f161396c8f28
keepFur <keepFur@163.com>
2018-07-05 17:03:45 +0800

    初始化项目

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,40 +11,40 @@ let gulp = require("gulp"),
     csscomb = require("gulp-csscomb"),
     del = require("del");
 
-gulp.task("clean", function (callback) {
+gulp.task("clean", function(callback) {
     del(["dest", "src/dest/", "src/dist/"], callback);
 });
 
-gulp.task("sass", function () {
+gulp.task("sass", function() {
     return gulp.src("src/sass/*").pipe(sass()).pipe(gulp.dest("src/css/"));
 });
 
-gulp.task("concat:css", ["sass"], function () {
+gulp.task("concat:css", ["sass"], function() {
     return gulp.src("src/css/*").pipe(concat("acs.all.css")).pipe(gulp.dest("src/dest/css/"));
 });
 
-gulp.task("cssmin", ["concat:css"], function () {
+gulp.task("cssmin", ["concat:css"], function() {
     return gulp.src("src/dest/css/acs.all.css").pipe(cssmin()).pipe(rename({ suffix: '.min' })).pipe(gulp.dest("src/dest/css/"));
 });
 
-gulp.task("sass:watch", ["sass"], function () {
+gulp.task("sass:watch", ["sass"], function() {
     return gulp.watch("src/sass/*.scss", ["sass"]);
 });
 
-gulp.task('copy', ["cssmin"], function () {
+gulp.task('copy', ["cssmin"], function() {
     return gulp.src('src/fonts/**')
         .pipe(gulp.dest('src/dest/fonts/'))
 });
 
-gulp.task('clear-flyer-min-js', function (callback) {
+gulp.task('clear-flyer-min-js', function(callback) {
     del(["src/plugins/js/flyer.all.min.js"], callback);
 });
 
-gulp.task('flyer-min-js', function () {
+gulp.task('flyer-min-js', function() {
     return gulp.src('src/plugins/js/flyer.all.js').pipe(uglify()).pipe(rename({ suffix: '.min' })).pipe(gulp.dest('src/plugins/js/'));
 });
 
-gulp.task('flyer-min-css', ["concat:css"], function () {
+gulp.task('flyer-min-css', ["concat:css"], function() {
     return gulp.src('src/plugins/css/flyer.all.css').pipe(cssmin()).pipe(rename({ suffix: '.min' })).pipe(gulp.dest('src/plugins/css/'));
 });
 
