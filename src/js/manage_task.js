9fd53124f1ed1bfe62fcb4b0ce8c0d909e475af7
keepFur <keepFur@163.com>
2018-07-09 16:01:30 +0800

    任务创建完成

---
+++ src/js/manage_task.js
@@ -0,0 +1,426 @@
+// 任务管理模块
+'use strict';
+flyer.define('task_manage', function(exports, module) {
+    var baseDatas = {
+        // 表格实例
+        $table: null,
+        pagerObj: null,
+        pageSizeSelectObj: null,
+        curIndex: Number(flyer.getQueryString('curIndex') || 1),
+        // 错误消息
+        paramErrMsg: '参数错误，请刷新页面重试',
+        netErrMsg: '系统已退出登录，请登录系统重试',
+        operatorErrMsg: {
+            single: '请选择一条数据操作',
+            batch: '请至少选择一条数据操作'
+        },
+        // 任务平台
+        taskPlant: ''
+    };
+
+    /**
+     *页面入口函数 
+     * 
+     */
+    function init() {
+        // 获取表格数据
+        getTableDatas(baseDatas.curIndex, 20);
+        // 初始化事件
+        initEvent();
+    }
+
+    /**
+     * 初始化DOM元素事件
+     * 
+     */
+    function initEvent() {
+        // 页签点击事件
+        $('#manageTaskTab li').on('click', function(event) {
+            var index = $(this).data('index') || 0;
+            if ($(this).hasClass('flyer-tab-active')) {
+                return false;
+            }
+            $('#manageTaskTab li').removeClass('flyer-tab-active');
+            $(this).addClass('flyer-tab-active');
+            baseDatas.taskPlant = $(this).data('task-plant');
+            getTableDatas(baseDatas.curIndex, 20);
+            return false;
+        });
+        // 创建任务
+        $('.createTask').on('click', createTaskHandle);
+        // 修改任务信息
+        $('.updateTask').on('click', updateTaskHandle);
+        // 禁用任务
+        $('.disabledTask').on('click', {
+            type: 0
+        }, toggleTaskHandle);
+        // 启用任务
+        $('.enabledTask').on('click', {
+            type: 1
+        }, toggleTaskHandle);
+    }
+
+    /**
+     * 创建任务的点击事件处理函数
+     * 
+     * @param {any} events 
+     */
+    function createTaskHandle(events) {
+        $(".flyer-layout-tree .flyer-layout-link[data-hash=task_create]").click();
+        return false;
+    }
+
+    /**
+     * 任务信息修改的点击事件处理函数
+     * @param {any} events 
+     */
+    function updateTaskHandle(events) {
+        var selectDatas = core.getTableCheckedDatas(baseDatas.$table);
+        if (selectDatas.length === 1) {
+            var plantName = selectDatas[0].plantName;
+            var description = selectDatas[0].description;
+            flyer.open({
+                content: `<div class="flyer-form-item dialog-form-container">
+                            <form class="flyer-form" name="plantUpdateForm">
+                                <div class="dialog-form-item">
+                                    <label>名称</label>
+                                    <input type="text" placeholder="可以是中文、字母、数字、下划线5-20位" class="flyer-input inline" name="plantName" value="${plantName}">
+                                </div>
+                                <div class="dialog-form-item">
+                                    <label>描述</label>
+                                    <input type="text" placeholder="简单的描述任务" class="flyer-input inline" name="description" value="${description}">
+                                </div>
+                            </form>
+                          </div>`,
+                isModal: true,
+                area: [400, 200],
+                title: '任务信息修改',
+                btns: [{
+                    text: '确定',
+                    click: function(ele) {
+                        var that = this;
+                        var plantInfo = core.getFormValues($('form[name=plantUpdateForm]'));
+                        var validTaskInfoResult = validTaskInfo(plantInfo);
+                        plantInfo.id = selectDatas[0].id;
+                        if (validTaskInfoResult.isPass) {
+                            $.ajax({
+                                url: '/api/updateTask',
+                                type: 'POST',
+                                data: plantInfo,
+                                beforeSend: function(jqXHR, settings) {
+                                    $.lockedBtn($(ele), true, ('修改中'));
+                                },
+                                success: function(data, textStatus, jqXHR) {
+                                    flyer.msg(data.success ? ('操作成功') : ('操作失败' + data.message));
+                                    that.close();
+                                    getTableDatas(1, 20);
+                                },
+                                error: function(jqXHR, textStatus, errorThrown) {
+                                    flyer.msg(baseDatas.errorMsg);
+                                },
+                                complete: function(jqXHR, textStatus) {
+                                    $.unlockBtn($(ele), ('确定'));
+                                }
+                            });
+                        } else {
+                            flyer.msg(validTaskInfoResult.msg);
+                        }
+                    }
+                }, {
+                    text: '取消',
+                    click: function() {
+                        this.close();
+                    }
+                }]
+            });
+        } else {
+            flyer.msg(baseDatas.operatorErrMsg.single);
+        }
+        return false;
+    }
+
+    /**
+     * 切换任务状态按钮点击事件处理函数
+     * 
+     * @param {any} events 
+     */
+    function toggleTaskHandle(events) {
+        var selectDatas = core.getTableCheckedDatas(baseDatas.$table);
+        var type = events.data.type;
+        var tipMsg = type === 0 ? '确定禁用吗？' : '确定启用吗？';
+        if (selectDatas.length === 1) {
+            flyer.confirm(tipMsg, function(result) {}, {
+                btns: [{
+                        text: '确定',
+                        click: function(elm) {
+                            this.close();
+                            $.ajax({
+                                url: '/api/toggleTask',
+                                type: 'POST',
+                                data: {
+                                    id: selectDatas[0].id,
+                                    status: type
+                                },
+                                success: function(data, textStatus, jqXHR) {
+                                    flyer.msg(data.success ? '操作成功' : ('操作失败' + data.message));
+                                    getTableDatas(1, 20);
+                                },
+                                error: function(jqXHR, textStatus, errorThrown) {
+                                    flyer.msg(baseDatas.netErrMsg);
+                                }
+                            });
+                        }
+                    },
+                    {
+                        text: ("取消"),
+                        click: function(elm) {
+                            this.close();
+                        }
+                    }
+                ],
+                title: "询问框",
+                isModal: true
+            });
+        } else {
+            flyer.msg(baseDatas.operatorErrMsg.single);
+        }
+        return false;
+    }
+
+    /**
+     * 渲染表格结构
+     * 
+     * @param {jq} $table 表格容器
+     * @param {Array} datas 表格数据
+     */
+    function renderTable($table, datas) {
+        if ($table && $table.length && Array.isArray(datas)) {
+            baseDatas.$table = flyer.table($table, {
+                columns: [{
+                    field: "",
+                    checkbox: true,
+                    styles: {
+                        width: 34
+                    }
+                }, {
+                    title: '数量',
+                    field: "",
+                    styles: {
+                        width: 50
+                    },
+                    formatter: function(row) {
+                        return '<i class="mdui-icon material-icons mdui-text-color-pink">&#xe417;</i>';
+                    }
+                }, {
+                    title: '任务类型',
+                    field: "",
+                    styles: {
+                        width: 200
+                    },
+                    formatter: function(row) {
+                        var taskPlant = row.taskPlant === 'TB' ? '淘宝' : '京东';
+                        return taskPlant + '（' + core.getTaskParentype(row.taskParentType) + '-' + core.getTaskChildType(row.taskChildType) + '）';
+                    }
+                }, {
+                    title: '任务名称',
+                    field: "taskName"
+                }, {
+                    title: '起止日期',
+                    field: "",
+                    formatter: function(row) {
+                        return flyer.formatDate('yyyy-mm-dd', row.taskStartDate) + '至' + flyer.formatDate('yyyy-mm-dd', row.taskEndDate);
+                    }
+                }, {
+                    title: '宝贝链接或淘口令',
+                    field: "taskBabyLinkToken"
+                }, {
+                    title: '总消费',
+                    field: "taskSumMoney",
+                    styles: {
+                        width: 80
+                    }
+                }, {
+                    title: '创建时间',
+                    field: "createdDate",
+                    styles: {
+                        width: 130
+                    },
+                    formatter: function(row, rows) {
+                        return flyer.formatDate('yyyy-mm-dd hh:MM', row.createdDate);
+                    }
+                }, {
+                    title: '最后修改时间',
+                    field: "updateDate",
+                    styles: {
+                        width: 130
+                    },
+                    formatter: function(row, rows) {
+                        return row.updateDate ? flyer.formatDate('yyyy-mm-dd hh:MM', row.updateDate) : '-';
+                    }
+                }, {
+                    title: '状态',
+                    field: 'status',
+                    styles: {
+                        width: 56
+                    },
+                    formatter: function(row) {
+                        return row.status === 1 ? '启用' : '停用';
+                    }
+                }],
+                data: datas
+            });
+        } else {
+            flyer.msg(baseDatas.paramErrMsg);
+        }
+    }
+
+    /**
+     * 初始化分页信息
+     * 
+     * @param {jq} $table 表格初始化之后的实例对象 
+     * @param {Array} datas 表格的数据
+     */
+    function randerDOMPager($table, datas, total, pagerObj) {
+        // 没有数据的时候
+        core.tableNoMatch($table, '暂时没有任务');
+        // 初始化下拉框，显示每页数据条数的下拉框
+        baseDatas.pageSizeSelectObj = core.initPagerSizeSelect($('#plantPagerSize'), core.getPageListByTotal(total), String(pagerObj.pageSize || 20), {
+            callback: getTableDatas,
+            pagerObj: baseDatas.pagerObj,
+            total: datas.total,
+            exports: exports
+        });
+        // 初始化分页
+        baseDatas.pagerObj = core.initPager($('.paper-container'), total, pagerObj.pageSize || 20, {
+            callback: getTableDatas,
+            pageNumber: pagerObj.pageNumber || 1,
+            pageSizeSelectObj: baseDatas.pageSizeSelectObj,
+            exports: exports
+        });
+        // 有数据的时候。才需要去初始化
+        if (datas.total) {
+            // 为表中的checkbox绑定点击事件
+            core.bindCheckboxEvent($table);
+        }
+    }
+
+    /**
+     * 设置spanner
+     * 
+     * @param {any} currentTotal 当前显示数据的总数
+     * @param {any} total 总数居
+     */
+    function setMountValue(currentTotal, total) {
+        $('#currentTaskMountSpan').text(currentTotal);
+        $('#taskMountSpan').text(total);
+    }
+
+    /**
+     * 获取表格数据
+     * 
+     * @param {Number} pageNumber 当前显示页数，默认为0
+     * @param {NUmber} pageSize 煤业显示的数据条数，默认为20
+     */
+    function getTableDatas(pageNumber, pageSize) {
+        var conditions = {
+                offset: pageNumber || 1,
+                limit: pageSize || 20,
+                nocache: window.Date.now(),
+                taskPlant: baseDatas.taskPlant
+            },
+            $table = $('#taskTable');
+        $.ajax({
+            url: '/api/readTaskPage',
+            type: 'GET',
+            data: conditions,
+            beforeSend: function(jqXHR, settings) {
+                $.addLoading();
+            },
+            success: function(data, jqXHR, textStatus) {
+                if (data.success) {
+                    renderTable($table, data.data.rows);
+                    randerDOMPager(baseDatas.$table, data.data.rows, data.data.total, {
+                        pageNumber: pageNumber || 1,
+                        pageSize: pageSize || 20
+                    });
+                    setMountValue(data.data.rows.length, data.data.total);
+                    core.bindCheckboxEvent(baseDatas.$table);
+                } else {
+                    flyer.msg(data.message);
+                    renderTable($table, []);
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                flyer.msg(baseDatas.netErrMsg);
+            },
+            complete: function(jqXHR, textStatus) {
+                $.removeLoading()
+            }
+        });
+    }
+
+    /**
+     * 根基用户的id获取用户的信息
+     * 
+     * @param {Number} id 用户id
+     * @param {funciton} callback 回调函数
+     */
+    function readTaskById(id, callback) {
+        if (!id) {
+            if (typeof callback === 'function') {
+                callback({
+                    success: false,
+                    message: '任务id不能为空'
+                });
+            }
+            return;
+        }
+        $.ajax({
+            url: '/api/readTaskById',
+            data: {
+                id: id
+            },
+            success: function(data) {
+                callback(data);
+            },
+            error: function(error) {
+                callback(data);
+            }
+        });
+    }
+
+    /**
+     * 校验任务信息
+     * 
+     * @param {Object} plantInfo 任务信息对象
+     */
+    function validTaskInfo(plantInfo) {
+        if (!plantInfo) {
+            return {
+                isPass: false,
+                msg: '参数错误'
+            }
+        }
+
+        if (!plantInfo.plantName) {
+            return {
+                isPass: false,
+                msg: '任务名称不能为空'
+            }
+        }
+
+        if (!plantInfo.description) {
+            return {
+                isPass: false,
+                msg: '任务描述不能为空'
+            }
+        }
+        return {
+            isPass: true,
+            msg: ''
+        };
+    }
+
+    // 页面入口
+    init();
+});
---
+++ src/js/task_create.js
@@ -0,0 +1,232 @@
+// 淘宝任务模块
+'use strict';
+$(function() {
+    // 页面入口
+    function init() {
+        initEvent();
+        initComponent();
+    }
+
+    /**
+     * 初始化组件
+     * 
+     */
+    function initComponent() {
+        // 日期组件
+        var taskStartDate = flyer.date($('#taskStartDate'), {
+            format: 'yyyy-mm-dd',
+            click: function(date) {
+                taskEndDate.setMinDate(date);
+                setTaskDetail({
+                    taskSumTime: core.computeDifferentDay(date, $('#taskEndDate').val())
+                });
+            }
+        });
+        var taskEndDate = flyer.date($('#taskEndDate'), {
+            format: 'yyyy-mm-dd',
+            click: function(date) {
+                taskStartDate.setMaxDate(date);
+                setTaskDetail({
+                    taskSumTime: core.computeDifferentDay($('#taskStartDate').val(), date)
+                });
+            }
+        });
+    }
+
+    /**
+     * 事件初始化
+     * 
+     */
+    function initEvent() {
+        // 任务平台的点击事件
+        $('form[name=taskForm]').find('input[name=taskPlant]').on('change', function(event) {
+            if ($('#taskTab li[data-index=2]').hasClass('flyer-tab-active')) {
+                $('input[value=APPOINTMENT_SOLD]').prop('disabled', $(this).val() === '1');
+                if ($(this).val() === '1' && $('input[value=APPOINTMENT_SOLD]').is(':checked')) {
+                    $('input[value=SEARCH_ADDCART]').prop('checked', true);
+                }
+            }
+            return false;
+        });
+        // 添加关键词和数量
+        $('button.js-add-keyword-quantity').on('click', addTaskKeywordQuantityHandle);
+        // 删除关键词和数量 
+        $('button.js-delete-keyword-quantity').on('click', deleteTaskKeywordQuantityHandle);
+        // 创建任务
+        $('#createTask').on('click', createTaskHandle);
+    }
+
+    /**
+     * 添加关键词
+     * 
+     * @param {any} event 
+     * @returns 
+     */
+    function addTaskKeywordQuantityHandle(event) {
+        var $items = $('.js-keyword-quantity-container').find('.js-keyword-quantity-item');
+        if ($items.length === 5) {
+            flyer.msg('主人，已经播了很多种了！');
+            return false;
+        }
+        var $container = $(event.target).parents('.js-keyword-quantity-container');
+        var $item = $(event.target).parents('.js-keyword-quantity-item').clone(true);
+        $container.append($item);
+        setKeywordAndQuantityIndex();
+        return false;
+    }
+
+    /**
+     * 设置数量和关键词的序号
+     * 
+     */
+    function setKeywordAndQuantityIndex() {
+        var $items = $('.js-keyword-quantity-container').find('.js-keyword-quantity-item');
+        $.each($items, function(index, item) {
+            var length = index + 1;
+            $(item).find('.js-task-keyword').text('关键词' + length);
+            $(item).find('.js-task-quantity').text('数量' + length);
+        });
+        return;
+    }
+
+    /**
+     * 删除关键词，必须保留一个
+     * 
+     * @param {any} event 
+     * @returns 
+     */
+    function deleteTaskKeywordQuantityHandle(event) {
+        var $items = $('.js-keyword-quantity-container').find('.js-keyword-quantity-item');
+        var $item = $(event.target).parents('.js-keyword-quantity-item');
+        if ($items.length === 1) {
+            flyer.msg('主人，必须留个种啊！');
+            return false;
+        }
+        $item.remove();
+        setKeywordAndQuantityIndex();
+        return false;
+    }
+
+    /**
+     * 创建任务
+     * 
+     * @param {any} event 
+     * @returns 
+     */
+    function createTaskHandle(event) {
+        var ele = event.target;
+        var $taskForm = $('form[name=taskForm]');
+        var taskInfo = getTaskInfo($taskForm);
+        var validTaskInfoResult = validTaskInfo(taskInfo);
+        if (validTaskInfoResult.isPass) {
+            $.ajax({
+                url: '/api/createTask',
+                type: 'POST',
+                data: taskInfo,
+                beforeSend: function(jqXHR, settings) {
+                    $.lockedBtn($(ele), true, '创建中');
+                },
+                success: function(data, textStatus, jqXHR) {
+                    if (data.success) {
+                        flyer.msg('操作成功');
+                        core.setWindowHash('manage_task');
+                    } else {
+                        flyer.msg('操作失败' + data.message);
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    flyer.msg(baseDatas.errorMsg);
+                },
+                complete: function(jqXHR, textStatus) {
+                    $.unlockBtn($(ele), '<i class="mdui-icon material-icons">&#xe569;</i>创建任务');
+                }
+            });
+        } else {
+            flyer.msg(validTaskInfoResult.msg);
+        }
+        return false;
+    }
+
+    /**
+     * 设置任务详情信息
+     * 
+     * @param {any} taskInfo 任务信息
+     */
+    function setTaskDetail(taskInfo) {
+        if (!taskInfo) {
+            $.writeLog('tb_task-setTaskDetail', '参数错误');
+            return;
+        }
+        // 任务耗时
+        if (taskInfo.taskSumTime) {
+            $('#taskSumTime').text(taskInfo.taskSumTime || 1);
+        }
+        // 任务单价
+        if (taskInfo.taskPrice) {
+            $('#taskPrice').text(taskInfo.taskPrice || 0);
+        }
+        // 任务总金额
+        if (taskInfo.taskSumMoney) {
+            $('#taskSumMoney').text(taskInfo.taskSumMoney || 0);
+        }
+        return;
+    }
+
+    /**
+     * 获取任务表单信息
+     * 
+     * @param {any} $form 任务表单
+     * @returns 任务表单信息对象
+     */
+    function getTaskInfo($form) {
+        var taskInfo = {};
+        taskInfo.taskUserId = 1;
+        taskInfo.taskParentType = $('#taskTab li.flyer-tab-active').data('task-parent-type');
+        taskInfo.taskUnitPrice = 10;
+        taskInfo.taskSumMoney = Math.random() * 10000;
+        taskInfo.keywordQuantity = getKeywordQuantity($form);
+        return $.extend(core.getFormValues($form), taskInfo);
+    }
+
+    /**
+     * 获取任务的关键词和数量
+     * 
+     * @param {any} $form 任务表单元素
+     */
+    function getKeywordQuantity($form) {
+        var result = [];
+        if (!$form) {
+            $.writeLog('tb_task-getKeywordQuantity', '参数错误');
+            return [];
+        }
+        var $items = $('.js-keyword-quantity-item');
+        $.each($items, function(index, item) {
+            result.push({
+                taskKeyword: $(item).find('input[name=taskKeyword]').val(),
+                taskQuantity: $(item).find('input[name=taskQuantity]').val()
+            });
+        });
+        return result;
+    }
+
+    /**
+     * 任务表单信息校验
+     * 
+     * @param {any} taskInfo 
+     */
+    function validTaskInfo(taskInfo) {
+        if (!taskInfo) {
+            $.writeLog('tb_task-setTaskDetail', '参数为空');
+            return {
+                isPass: false,
+                msg: '参数不能为空'
+            };
+        }
+        return {
+            isPass: true,
+            msg: ''
+        };
+    }
+
+    init();
+});
