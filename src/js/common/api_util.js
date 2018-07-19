f27948695d5672dc7307f18d31bbfdba0d9dbc7a
keepFur <keepFur@163.com>
2018-07-19 20:22:18 +0800

    创建任务对接成功

---
+++ src/js/common/api_util.js
@@ -0,0 +1,140 @@
+"use strict";
+var APIUtil = {
+    domain: 'http://api.lieliu.com:1024',
+    userKey: 'c1e5606f7b40e680d3b5bfc7dbb042ff',
+    getServerTimestamp: function(callback) {
+        var url = this.domain + '/api/sys_now?format=json';
+        callback = callback || function(res) {
+            flyer.msg('sys_now==>' + res.data.time);
+        };
+        $.get(url, function(data) {
+            callback(data);
+        });
+    },
+    generateOrderNumer: function() {
+        let random = Math.ceil(Math.random() * 1000000);
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = core.padStart((date.getMonth() + 1));
+        let day = core.padStart(date.getDate());
+        let hours = core.padStart(date.getHours());
+        let minutes = core.padStart(date.getMinutes());
+        let seconds = core.padStart(date.getSeconds());
+        let mill = core.padStart(date.getMilliseconds());
+        return '' + year + month + day + hours + minutes + seconds + random;
+    },
+    signkey: function(apiName, params) {
+        console.log('原串：' + params);
+        var encodeUlr = encodeURIComponent(`${apiName}?${params}&${this.userKey}`);
+        console.log('encodeurl之后：' + encodeUlr);
+        return $.md5(encodeUlr);
+    },
+    createTask: function(params, callback) {
+        var url = this.domain + '/ll/task_add';
+        var orderNumber = this.generateOrderNumer();
+        var _this = this;
+        this.getServerTimestamp(function(data) {
+            params = $.extend(params, {
+                format: 'json',
+                ver: 4,
+                timestamp: data.data.time,
+                id: orderNumber,
+                username: 'u_1657222',
+            });
+            params.signkey = _this.signkey('/ll/task_add', core.objectToString(params));
+            console.log('MD5之后：' + params.signkey);
+            console.log('发送给服务器的参数：' + core.objectToString(params));
+            $.ajax({
+                url: url,
+                type: 'get',
+                data: params,
+                success: function(res) {
+                    res.orderNumber = orderNumber;
+                    callback(res);
+                },
+                error: function(error) {
+                    callback({}, error);
+                }
+            });
+        });
+    },
+    // 查询任务
+    listTask: function(params, callback) {
+        var url = this.domain + '/ll/task_list';
+        var orderNumber = this.generateOrderNumer();
+        var signKey = '';
+        var _this = this;
+        this.getServerTimestamp(function(data) {
+            params = $.extend(params, {
+                format: 'json',
+                timestamp: data.data.time,
+                username: 'u_1657222',
+                ver: 4
+            });
+            params.signkey = _this.signkey('/ll/task_list', core.objectToString(params));
+            $.ajax({
+                url: 'http://api.lieliu.com:1024/ll/task_list',
+                data: params,
+                success: function(res) {
+                    callback(res);
+                },
+                error: function(error) {
+                    flyer.msg(error.message)
+                }
+            });
+        });
+    },
+    // 取消任务
+    cancelTask: function(orderNumber, callback) {
+        var url = this.domain + '/ll/task_cancel';
+        var signKey = '';
+        var _this = this;
+        this.getServerTimestamp(function(data) {
+            var params = {
+                format: 'json',
+                timestamp: data.data.time,
+                username: 'u_1657222',
+                ver: 4,
+                id: orderNumber
+            };
+            params.signkey = _this.signkey('/ll/task_cancel', core.objectToString(params));
+            $.ajax({
+                url: url,
+                data: params,
+                success: function(res) {
+                    callback(res);
+                },
+                error: function(error) {
+                    flyer.msg(error.message)
+                }
+            });
+        });
+    },
+    // 暂停或者恢复任务
+    pauseAndResumeTask: function(orderNumber, status, callback) {
+        var url = this.domain + '/ll/task_pause';
+        var signKey = '';
+        var _this = this;
+        this.getServerTimestamp(function(data) {
+            var params = {
+                format: 'json',
+                timestamp: data.data.time,
+                username: 'u_1657222',
+                ver: 4,
+                id: orderNumber,
+                status: status
+            };
+            params.signkey = _this.signkey('/ll/task_pause', core.objectToString(params));
+            $.ajax({
+                url: url,
+                data: params,
+                success: function(res) {
+                    callback(res);
+                },
+                error: function(error) {
+                    flyer.msg(error.message)
+                }
+            });
+        });
+    }
+};
--- src/js/task_create.js
+++ src/js/task_create.js
@@ -1,123 +1,14 @@
 // 创建任务模块
 'use strict';
 $(function() {
-    var APIUtil = {
-        domain: 'http://api.lieliu.com:1024',
-        userKey: 'c1e5606f7b40e680d3b5bfc7dbb042ff',
-        getServerTimestamp: function(callback) {
-            var url = this.domain + '/api/sys_now?format=json';
-            callback = callback || function(res) {
-                flyer.msg('sys_now==>' + res.data.time);
-            };
-            $.get(url, function(data) {
-                callback(data);
-            });
-        },
-        generateOrderNumer: function() {
-            let random = Math.ceil(Math.random() * 1000000);
-            let date = new Date();
-            let year = date.getFullYear();
-            let month = core.padStart((date.getMonth() + 1));
-            let day = core.padStart(date.getDate());
-            let hours = core.padStart(date.getHours());
-            let minutes = core.padStart(date.getMinutes());
-            let seconds = core.padStart(date.getSeconds());
-            let mill = core.padStart(date.getMilliseconds());
-            return '' + year + month + day + hours + minutes + seconds + random;
-        },
-        signkey: function(apiName, params) {
-            console.log(params);
-            var encodeUlr = encodeURIComponent(`${apiName}?${params}&${this.userKey}`);
-            return $.md5(encodeUlr);
-        },
-        createTask: function(params, callback) {
-            var url = this.domain + '/ll/task_add';
-            var orderNumber = this.generateOrderNumer();
-            var _this = this;
-            this.getServerTimestamp(function(data) {
-                params = $.extend(params, {
-                    format: 'json',
-                    ver: 4,
-                    timestamp: data.data.time,
-                    id: orderNumber,
-                    username: 'u_1657222',
-                });
-                console.log(params);
-                params.signkey = _this.signkey('/ll/task_add', core.objectToString(params));
-                $.ajax({
-                    url: url,
-                    data: params,
-                    success: function(res) {
-                        res.orderNumber = orderNumber;
-                        callback(res);
-                    },
-                    error: function(error) {
-                        callback({}, error);
-                    }
-                });
-                // target: `https://item.taobao.com/item.htm?spm=a211pk.steins68998.wb-qs-fp-20180312-ruiyu-video-pc6.3.129fjVNljVNl6G&id=39878007598`,
-            });
-        },
-        // 查询任务
-        listTask: function(params, callback) {
-            var url = this.domain + '/ll/task_list';
-            var orderNumber = this.generateOrderNumer();
-            var signKey = '';
-            var _this = this;
-            this.getServerTimestamp(function(data) {
-                params = $.extend(params, {
-                    format: 'json',
-                    timestamp: data.data.time,
-                    username: 'u_1657222',
-                    ver: 4
-                });
-                params.signkey = _this.signkey('/ll/task_list', core.objectToString(params));
-                $.ajax({
-                    url: 'http://api.lieliu.com:1024/ll/task_list',
-                    data: params,
-                    success: function(res) {
-                        callback(res);
-                    },
-                    error: function(error) {
-                        flyer.msg(error.message)
-                    }
-                });
-            });
-        },
-        // 取消任务
-        cancelTask: function(orderNumber, callback) {
-            var url = this.domain + '/ll/task_cancel';
-            var signKey = '';
-            var _this = this;
-            this.getServerTimestamp(function(data) {
-                var params = {
-                    format: 'json',
-                    timestamp: data.data.time,
-                    username: 'u_1657222',
-                    ver: 4,
-                    id: orderNumber
-                };
-                params.signkey = _this.signkey('/ll/task_cancel', core.objectToString(params));
-                $.ajax({
-                    url: url,
-                    data: params,
-                    success: function(res) {
-                        callback(res);
-                    },
-                    error: function(error) {
-                        flyer.msg(error.message)
-                    }
-                });
-            });
-        }
-    };
     var userInfo = $('#userName').data('user');
     // 页面入口
     function init() {
         initEvent();
         initComponent();
         core.getUserInfoById(userInfo.id, function(user) {
-            userInfo = user;
+            userInfo = user.data.rows[0];
+            console.log(userInfo);
             $('#userName').data('user', JSON.stringify(user));
         });
     }
@@ -257,6 +148,7 @@ $(function() {
                 }
                 // 调用第三方api的时候，生成的订单号，需要传回到数据库中，不能再次生成
                 taskInfo.taskOrderNumber = res.orderNumber;
+                taskInfo.taskKeyword = replaceSByDotted(taskInfo.taskKeyword);
                 $.ajax({
                     url: '/api/createTask',
                     type: 'POST',
@@ -271,7 +163,7 @@ $(function() {
                             core.setWindowHash('manage_task');
                         } else {
                             // 操作失败需要取消当前任务
-                            APIUtil.cancelTask(taskOrderNumber, function(res) {
+                            APIUtil.cancelTask(taskInfo.taskOrderNumber, function(res) {
                                 flyer.msg('操作失败：' + res.data.tips);
                             });
                         }
@@ -343,10 +235,10 @@ $(function() {
         signKeyTaskInfo.type = core.getTypeCodeByValue(taskInfo.taskChildType).code;
         signKeyTaskInfo.count = taskInfo.taskQuantity;
         signKeyTaskInfo.target = taskInfo.taskBabyLinkToken;
-        signKeyTaskInfo.keyword = taskInfo.taskKeyword;
+        signKeyTaskInfo.keyword = replaceSByDotted(taskInfo.taskKeyword);
         signKeyTaskInfo.sUrl = taskInfo.sUrl;
-        signKeyTaskInfo.hour = computeEqualPart(taskInfo.taskQuantity, computeMainHourToday());
-        signKeyTaskInfo.hour = new Array(24).fill(1);
+        signKeyTaskInfo.hour = computeEqualPart(taskInfo.taskQuantity, computeMainHourToday()).join(',');
+        // signKeyTaskInfo.hour = new Array(24).fill(1);
         signKeyTaskInfo.goodsBrowsingTime = taskInfo.taskGoodsBrowsingTime;
         return signKeyTaskInfo;
     }
@@ -474,6 +366,15 @@ $(function() {
     function computeMainHourToday() {
         var hour = new Date().getHours();
         return 24 - hour;
+    }
+
+    // 将字符串中的空格使用分号代替，先要去除两端空格
+    function replaceSByDotted(str) {
+        if (!str && typeof str !== 'string') {
+            $.writeLog('task_create-replaceSByDotted', ' 参数类型错误');
+            return '';
+        }
+        return $.trim(str).replace(/(\s|，)+/g, ',');
     }
     init();
 });
