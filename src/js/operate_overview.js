265fb3b083d5d762031440c20229487b48fb30ed
joey <joey.su@huolala.cn>
2018-11-16 10:10:16 +0800

    网站统计功能

--- src/js/operate_overview.js
+++ src/js/operate_overview.js
@@ -9,6 +9,7 @@ layui.use(['element', 'layer'], function () {
      */
     (function init() {
         getTodayData();
+        getAllData();
         getAddMoney();
         getOrderData();
         getUserData();
@@ -32,117 +33,104 @@ layui.use(['element', 'layer'], function () {
         });
     }
 
-
     /**
      *获取今日的网站数据
      *
-     * @param {*} date
      */
-    function getTodayData(date) {
-        date = date || 7;
-        var numDayDate = getNumDayDate(date);
-        var createdDateStart = numDayDate[0];
-        var createdDateEnd = numDayDate[date - 1];
+    function getTodayData() {
+        $.get('/api/getTodayData', function (res) {
+            if (res.success) {
+                //负值
+                var data = res.data;
+                $('.js-user-today-add-score').text(core.fenToYuan(data.todayAddSumMoney || 0) + '元/' + data.todayAddMoneyCount + '笔');
+                $('.js-user-today-add-count').text(data.todayAddUserCount);
+                $('.js-user-today-kb-count').text(core.fenToYuan(data.todayAddKbOrderSumMoney || 0) + '元/' + data.todayAddKbOrderCount + '笔');
+                $('.js-user-today-task-count').text(core.fenToYuan(data.todayAddTaskOrderSumMoney || 0) + '元/' + data.todayAddTaskOrderCount + '笔');
+            } else {
+                layer.msg(res.message);
+            }
+        }, 'json');
+    }
 
+    /**
+     *获取全部的网站数据
+     *
+     */
+    function getAllData() {
+        $.get('/api/getAllData', function (res) {
+            if (res.success) {
+                //负值
+                var data = res.data;
+                $('.js-user-all-add-score').text(core.fenToYuan(data.allAddSumMoney || 0) + '元/' + data.allAddMoneyCount + '笔');
+                $('.js-user-all-add-count').text(data.allAddUserCount);
+                $('.js-user-all-kb-count').text(core.fenToYuan(data.allAddKbOrderSumMoney || 0) + '元/' + data.allAddKbOrderCount + '笔');
+                $('.js-user-all-task-count').text(core.fenToYuan(data.allAddTaskOrderSumMoney || 0) + '元/' + data.allAddTaskOrderCount + '笔');
+            } else {
+                layer.msg(res.message);
+            }
+        }, 'json');
     }
 
     /**
      * 获取余额充值的数据
      * 
-     * @param {any} date 
+     * @param {any} day
      */
-    function getAddMoney(date) {
-        date = date || 7;
-        var numDayDate = getNumDayDate(date);
-        var createdDateStart = numDayDate[0];
-        var createdDateEnd = numDayDate[date - 1];
-        $.get('/api/readAddMoneyOfInTime', {
-            createdDateStart: createdDateStart,
-            createdDateEnd: createdDateEnd
+    function getAddMoney(day) {
+        day = day || 7;
+        $.get('/api/getAddMoney', {
+            day: day
         }, function (res) {
-            var rows = res.data.rows;
-            numDayDate.forEach(function (item) {
-                if (!hasContainerEle(item, rows)) {
-                    rows.push({
-                        createdDate: $.formatDate('yyyy/mm/dd', item),
-                        count: 0
-                    });
-                }
-            });
-            rows = rows.map(function (item, index) {
-                return {
-                    createdDate: $.formatDate('yyyy/mm/dd', item.createdDate),
-                    count: item.count
-                };
-            });
-            rows.sort(function (item1, item2) {
-                return new Date(item1.createdDate).getTime() - new Date(item2.createdDate).getTime();
-            });
-        });
+            if (res.success) {
+                //负值
+                var data = res.data;
+                $('.js-user-add-score').text(core.fenToYuan(data.addSumMoney || 0) + '元/' + data.addMoneyUserCount + '笔');
+                $('.js-user-sub-score').text(core.fenToYuan(data.subSumMoney || 0) + '元/' + data.subMoneyUserCount + '笔');
+            } else {
+                layer.msg(res.message);
+            }
+        }, 'json');
     }
 
     /**
      *获取某一个时间段的订单数据
      *
      */
-    function getOrderData(date) {
-        date = date || 7;
-        var numDayDate = getNumDayDate(date);
-        var createdDateStart = numDayDate[0];
-        var createdDateEnd = numDayDate[date - 1];
+    function getOrderData(day) {
+        day = day || 7;
+        $.get('/api/getOrderData', {
+            day: day
+        }, function (res) {
+            if (res.success) {
+                //负值
+                var data = res.data;
+                $('.js-user-kb-order').text(core.fenToYuan(data.kbOrderSumMoney || 0) + '元/' + data.kbOrderCount + '笔');
+                $('.js-user-task-order').text(core.fenToYuan(data.taskOrderSumMoney || 0) + '元/' + data.taskOrderCount + '笔');
+            } else {
+                layer.msg(res.message);
+            }
+        }, 'json');
     }
 
     /**
      *获取某一个时间的用户数据
      *
      */
-    function getUserData(date) {
-        date = date || 7;
-        var numDayDate = getNumDayDate(date);
-        var createdDateStart = numDayDate[0];
-        var createdDateEnd = numDayDate[date - 1];
-    }
-
-    /**
-     * 获取最近num天的日期
-     * 并返回一个数组
-     * 
-     * @param {any} num 最近的天数 默认是七天
-     */
-    function getNumDayDate(num) {
-        num = num || 7;
-        if (isNaN(num)) {
-            console.log(num);
-            return [];
-        }
-        // 根据时间戳来计算
-        var todayTime = new Date().getTime();
-        var sumTime = num * 24 * 60 * 60 * 1000;
-        var startTime = todayTime - sumTime;
-        var oneDayTime = 24 * 60 * 60 * 1000;
-        var result = [];
-        for (var i = 0; i < num; i++) {
-            result[i] = $.formatDate('yyyy/mm/dd', new Date(startTime + (i + 1) * oneDayTime));
-        }
-        return result;
-    }
-
-    /**
-     * 判断某一个元素是否在数组中
-     * 
-     * @param {any} ele 
-     * @param {any} arr 
-     */
-    function hasContainerEle(ele, arr) {
-        var has = false;
-        if (ele && arr && Array.isArray(arr)) {
-            arr.forEach(function (element) {
-                if ($.formatDate('yyyy/mm/dd', element.createdDate) === ele) {
-                    has = true;
-                    return false;
-                }
-            }, this);
-        }
-        return has;
+    function getUserData(day) {
+        day = day || 7;
+        $.get('/api/getUserData', {
+            day: day
+        }, function (res) {
+            if (res.success) {
+                //负值
+                var data = res.data;
+                $('.js-user-add-count').text(data.addUserCount);
+                $('.js-user-gold-count').text(data.goldUserCount);
+                $('.js-user-common-count').text(data.commonUserCount);
+                $('.js-user-inner-count').text(data.innerUserCount);
+            } else {
+                layer.msg(res.message);
+            }
+        }, 'json');
     }
 });
