30b87c4f3a51fd5bfd9266bbc99400f43e949e78
keepFur <keepFur@163.com>
2018-11-26 07:57:24 +0800

    推广链接

--- src/js/add_money.js
+++ src/js/add_money.js
@@ -1,6 +1,6 @@
 //  用户充值模块
 "use strict";
-layui.use(['element', 'layer', 'form'], function () {
+layui.use(['element', 'layer', 'form'], function() {
     var element = layui.element;
     var layer = layui.layer;
     var form = layui.form;
@@ -16,8 +16,10 @@ layui.use(['element', 'layer', 'form'], function () {
     function initComponment() {
         // 设置账号信息以及用户余额
         $('form[name=addMoneyForm] input[name=userName]').val($('#userName').text().trim());
-        core.getUserInfoById(function (res) {
+        core.getUserInfoById(function(res) {
             $('#userMainMoney').text(core.fenToYuan(res.data.rows[0].money));
+            // 生成推广链接（注册的时候自动给账号分配一个推广链接），链接形式：www.08v12.com?otherShareCode=adadjasjdiujasdiqwkajskd
+            $('input[name=shareLink]').val(location.host + '?otherShareCode=' + res.data.rows[0].myShareCode);
         });
         // 获取所有的充值方式并渲染
         getPackageDatas();
@@ -28,6 +30,8 @@ layui.use(['element', 'layer', 'form'], function () {
     function initEvent() {
         // 立即充值
         $('#addMoney').on('click', addMoneyHandle);
+        // 一键复制推广文案
+        $('#copyShareLink').on('click', copyShareLinkHandle);
     }
 
     // 充值套餐点击函数
@@ -43,7 +47,7 @@ layui.use(['element', 'layer', 'form'], function () {
         var collectCount = sumMoney / collectPrice;
         // 总积分/除以单价 再取整
         $('#purchaseMoney').text(core.numberToLocalString(sumMoney));
-        $('#purchaseScore').text(core.numberToLocalString(sumScore));// 总积分
+        $('#purchaseScore').text(core.numberToLocalString(sumScore)); // 总积分
         // $('#presentScore').text(core.numberToLocalString(presentScore));
         $('#trafficCount').text(core.numberToLocalString(trafficCount));
         $('#collectCount').text(core.numberToLocalString(collectCount));
@@ -71,14 +75,14 @@ layui.use(['element', 'layer', 'form'], function () {
             title: '在线充值付款',
             btn: '支付完成',
             scrollbar: false,
-            yes: function (index, layero) {
+            yes: function(index, layero) {
                 $.lockedBtn(layero.find('.layui-layer-btn0'), true, '支付状态检测中');
                 var count = 0;
                 clearInterval(open.timer);
-                open.timer = setInterval(function () {
+                open.timer = setInterval(function() {
                     count++;
                     if (count < 5) {
-                        getQrCodePayStatus(open.qr_id, open.addPackageType, function (payStatus) {
+                        getQrCodePayStatus(open.qr_id, open.addPackageType, function(payStatus) {
                             layer.closeAll('msg');
                             if (payStatus) {
                                 open.payStatus = true;
@@ -99,10 +103,10 @@ layui.use(['element', 'layer', 'form'], function () {
                     }
                 }, count === 0 ? 0 : 10000);
             },
-            cancel: function () {
+            cancel: function() {
                 // 支付成功之后不需要再去检查支付状态了
                 if (!open.payStatus) {
-                    getQrCodePayStatus(open.qr_id, open.addPackageType, function (payStatus) {
+                    getQrCodePayStatus(open.qr_id, open.addPackageType, function(payStatus) {
                         layer.closeAll('msg');
                         if (payStatus) {
                             layer.msg('支付成功');
@@ -114,7 +118,7 @@ layui.use(['element', 'layer', 'form'], function () {
                     });
                 }
             },
-            success: function (layero, index) {
+            success: function(layero, index) {
                 // 设置付款金额
                 $('#payMount').text(checkedPackage.data('purchase-money') / 100);
                 $.ajax({
@@ -125,10 +129,10 @@ layui.use(['element', 'layer', 'form'], function () {
                         packageId: checkedPackage.val(),
                         qr_type: 'QR_TYPE_NOLIMIT'
                     },
-                    beforeSend: function () {
+                    beforeSend: function() {
                         $.addLoading($('.js-pay-container'));
                     },
-                    success: function (data) {
+                    success: function(data) {
                         $('#jsPayCodeImg').toggle(data.success);
                         $('#jsPayCodeRefresh').toggle(!data.success);
                         if (data.success) {
@@ -136,11 +140,11 @@ layui.use(['element', 'layer', 'form'], function () {
                             open.qr_id = data.data.qr_id;
                             open.addPackageType = $('input[type=radio][name=addPackageType]').val();
                             var count = 0;
-                            open.timer = setInterval(function () {
+                            open.timer = setInterval(function() {
                                 $.lockedBtn(layero.find('.layui-layer-btn0'), true, '支付状态检测中');
                                 count++;
                                 if (count < 5) {
-                                    getQrCodePayStatus(open.qr_id, open.addPackageType, function (payStatus) {
+                                    getQrCodePayStatus(open.qr_id, open.addPackageType, function(payStatus) {
                                         layer.closeAll('msg');
                                         if (payStatus) {
                                             open.payStatus = true;
@@ -162,10 +166,10 @@ layui.use(['element', 'layer', 'form'], function () {
                             }, 10000);
                         }
                     },
-                    error: function () {
+                    error: function() {
                         layer.msg(baseDatas.netErrMsg);
                     },
-                    complete: function () {
+                    complete: function() {
                         $.removeLoading($('.js-pay-container'));
                     }
                 });
@@ -184,14 +188,14 @@ layui.use(['element', 'layer', 'form'], function () {
                 status: 1,
                 offset: 1
             },
-            beforeSend: function (jqXHR, settings) {
+            beforeSend: function(jqXHR, settings) {
                 $.addLoading();
             },
-            success: function (data, jqXHR, textStatus) {
+            success: function(data, jqXHR, textStatus) {
                 if (data.success) {
                     renderPackage(data.data.rows);
                     // 充值套餐change事件并且模拟触发一次change事件
-                    form.on('radio', function (data) {
+                    form.on('radio', function(data) {
                         if (this.name === 'addPackageType') {
                             addPackageTypeHandle(data.elem);
                         }
@@ -202,10 +206,10 @@ layui.use(['element', 'layer', 'form'], function () {
                     renderPackage([]);
                 }
             },
-            error: function (jqXHR, textStatus, errorThrown) {
+            error: function(jqXHR, textStatus, errorThrown) {
                 layer.msg(baseDatas.netErrMsg);
             },
-            complete: function (jqXHR, textStatus) {
+            complete: function(jqXHR, textStatus) {
                 $.removeLoading()
             }
         });
@@ -214,7 +218,7 @@ layui.use(['element', 'layer', 'form'], function () {
     // 渲染所有的充值方式
     function renderPackage(packages) {
         var $container = `<div class="layui-input-block">`;
-        $.each(packages, function (index, pack) {
+        $.each(packages, function(index, pack) {
             if (index % 4 === 0 && index !== 0) {
                 $container += '</div><div class="layui-input-block">';
                 $container += generateRadio(pack, index === 0);
@@ -256,9 +260,28 @@ layui.use(['element', 'layer', 'form'], function () {
                 addPackageType: addPackageType,
                 orderNumber: APIUtil.generateOrderNumer()
             },
-            success: function (payStatus) {
+            success: function(payStatus) {
                 callback(payStatus.data.status);
             }
         });
+    }
+
+    /**
+     * 一键复制推广文案按钮的点击事件的处理函数
+     * 每个账户下有一个otherShareCode（别人分享的）和myShareCode（自己的）：
+     * 当用户注册的时候：判断链接中是否有otherShareCode查询参数，如果有的话，说明是通过别人分享的链接进行注册的，否则不是。注册的时候，每个账户分配一个myShareCode，当用户进行充值的时候，根据用户的otherShareCode去判断是否有人通过你分享的链接进行了注册，如果有的话，根据充值金额，按照比例进行佣金的返回并记录日志。
+     * 实现步骤（服务端）：
+     * 1，user 表增加myShareCode和otherShareCode字断
+     * 2，注册的时候，判断查询参数，是否有otherShareCode，如果有写入到otherShareCode字断中，并自动生成一个myOtherCode
+     * 3，充值的时候，判断是否是通过别人的链接进行注册的，如果是，返回佣金和记录日志
+     * 实现步骤（客户端）：
+     * 1，获取用户信息（myShareCode），生成推广链接
+     * 2，组装推广文案
+     * 3，复制推广文案
+     * @param {any} e 
+     */
+    function copyShareLinkHandle(e) {
+        layer.msg('推广文案已成功复制到粘贴吧，去推广吧');
+        return false;
     }
 });
